931e36a21ac37a9b46521b588b20fc0c879835cd
ui-jb <bayer@urbaninsight.com>
2025-02-21 11:37:31 -0600

    Copy over changes from prototype

---
+++ src/components/attract-mode/attract-mode-content.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+const ATTRACT_MODE_TIMEOUT_MINUTES = 3;
+const ATTRACT_MODE_TIMEOUT_MILLISECONDS =
+  ATTRACT_MODE_TIMEOUT_MINUTES * 60 * 1000;
+
+export default function AttractModeContent() {
+  // TODO: move attract mode state to global context
+  const [attractModeActive, setAttractModeActive] = useState(true);
+  const inactivityTimerRef = useRef<NodeJS.Timeout | null>(null); // Use a ref for the timer
+
+  // Function to start the attract mode
+  const startAttractMode = () => {
+    setAttractModeActive(true);
+    // Add logic to play your attract mode video or animation here
+    console.log("Attract mode started");
+  };
+
+  // Function to stop the attract mode
+  const stopAttractMode = () => {
+    setAttractModeActive(false);
+    // Add logic to stop your attract mode video or animation here
+    console.log("Attract mode stopped");
+  };
+
+  // Reset the inactivity timer
+  const resetInactivityTimer = () => {
+    if (inactivityTimerRef.current) {
+      clearTimeout(inactivityTimerRef.current);
+    }
+
+    inactivityTimerRef.current = setTimeout(
+      startAttractMode,
+      ATTRACT_MODE_TIMEOUT_MILLISECONDS
+    );
+  };
+
+  // Handle user interaction
+  const handleUserInteraction = () => {
+    if (attractModeActive) {
+      stopAttractMode();
+    }
+    resetInactivityTimer();
+  };
+
+  // Set up event listeners for user interaction
+  useEffect(() => {
+    const events = ["click", "touchstart", "keydown"];
+    events.forEach((event) => {
+      window.addEventListener(event, handleUserInteraction);
+    });
+
+    // Start the initial inactivity timer
+    resetInactivityTimer();
+
+    // Clean up event listeners and timer
+    return () => {
+      events.forEach((event) => {
+        window.removeEventListener(event, handleUserInteraction);
+      });
+      if (inactivityTimerRef.current) {
+        clearTimeout(inactivityTimerRef.current);
+      }
+    };
+  }, [attractModeActive]);
+
+  // TODO: Fade out
+  // TODO: Reset IIIF to default state on animation end
+  return attractModeActive ? (
+    <div className="attract-mode absolute top-0 left-0 h-[100dvh] w-[100dvw] flex flex-col justify-center items-center gap-y-8 bg-black text-white z-[10] cursor-pointer">
+      {/* Video BG */}
+      <div className="absolute top-0 left-0 h-[100dvh] w-[100dvw]">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="object-cover h-full w-full"
+        >
+          {/* TODO: make dynamic */}
+          <source src="/test_vid.mp4" type="video/mp4" />
+        </video>
+      </div>
+
+      {/* Darkened Overlay over Video so text is more legible */}
+      <div className="absolute top-0 left-0 h-[100dvh] w-[100dvw] bg-black opacity-30 z-[10]"></div>
+
+      {/* TODO: make dynamic - use label component? */}
+      <h1 className="text-7xl font-bold animate-pulse text-center max-w-6xl z-[11]">
+        Commedia dell&apos;Arte: The Masks of Antonio Fava
+      </h1>
+      <p className="animate-pulse text-3xl font-bold uppercase border-4 border-white px-5 py-2 hover:text-gray-500 hover:border-gray-500 mb-5">
+        Tap to Begin
+      </p>
+    </div>
+  ) : null;
+}
---
+++ src/components/iiif-viewer/iiif-viewer.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+// TODO: Loading skeleton
+const Viewer = dynamic(
+  () => import("@samvera/clover-iiif").then((mod) => mod.Viewer),
+  {
+    ssr: false, // Disable server-side rendering for this component
+    loading: () => <p>Loading viewer...</p>, // Optional: Add a loading fallback
+  }
+);
+
+export default function IIIFViewer() {
+  // TODO: base content off of ENV variable?
+  const iiifContent = `${process.env.NEXT_PUBLIC_URL}/one-image-example-manifest.json`;
+
+  // TODO: base colors off of ENV variables?
+  const customTheme = {
+    colors: {
+      /**
+       * Black and dark grays in a light theme.
+       * All must contrast to 4.5 or greater with `secondary`.
+       */
+      primary: "#095CFF",
+      primaryMuted: "#433A3F",
+      primaryAlt: "#211C1F",
+
+      /**
+       * Key brand color(s).
+       * `accent` must contrast to 4.5 or greater with `secondary`.
+       */
+      accent: "#095CFF",
+      accentMuted: "#1E69FB",
+      accentAlt: "#1E69FB",
+
+      /**
+       * White and light grays in a light theme.
+       * All must must contrast to 4.5 or greater with `primary` and  `accent`.
+       */
+      secondary: "#000",
+      secondaryMuted: "#ECEFF1",
+      secondaryAlt: "#CFD8DC",
+    },
+    fonts: {
+      sans: "'Helvetica Neue', sans-serif",
+      display: "Optima, Georgia, Arial, sans-serif",
+    },
+  };
+
+  return (
+    <div className="h-[100dvh] w-[100dvw] flex flex-col justify-center items-center bg-black text-white">
+      <Viewer
+        iiifContent={iiifContent}
+        customTheme={customTheme}
+        options={{
+          canvasBackgroundColor: "#000",
+          canvasHeight: "100%",
+          showIIIFBadge: false,
+          showTitle: false,
+          // TODO: ENV variable to hide info panel toggle?
+          informationPanel: {
+            open: false,
+            renderToggle: true,
+            renderAbout: false,
+            renderAnnotation: true,
+          },
+          openSeadragon: {
+            scrollZoom: true,
+            navigatorRotate: false,
+            showRotationControl: false,
+            showHomeControl: false,
+            showFullPageControl: false,
+            navigatorDisplayRegionColor: "transparent",
+            gestureSettingsMouse: {
+              scrollToZoom: true,
+            },
+          },
+        }}
+      />
+    </div>
+  );
+}
