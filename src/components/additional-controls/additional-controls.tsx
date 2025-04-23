95f74070e380bbe0028a23afa240f070ac6e0672
ui-jb <bayer@urbaninsight.com>
2025-04-23 14:47:25 -0500

    Add "learn more" icon, clean up "Pinch to zoom" indicator in controls section

--- src/components/additional-controls/additional-controls.tsx
+++ src/components/additional-controls/additional-controls.tsx
@@ -1,5 +1,6 @@
 import { MuseumObjectContext } from "@/context/museum-object-context";
 import { useContext } from "react";
+import InfoSpeechBubbleIcon from "../icons/info-speech-bubble";
 import PinchToZoomIcon from "../icons/pinch-to-zoom";
 import LanguageButton from "../language-button/language-button";
 
@@ -20,7 +21,7 @@ export default function AdditionalControls() {
 
   return (
     <div className="flex flex-row items-center justify-center space-x-[0.618rem] rounded-lg p-4">
-      <div className="w-max-content mr-8 flex h-8 items-center justify-center rounded-md bg-black px-[18px] py-[15px] text-white">
+      <div className="w-max-content mr-8 flex h-10 items-center justify-center rounded-md bg-black px-[18px] py-[15px] text-white">
         <span className="mr-4 text-lg">
           <PinchToZoomIcon className="h-7 w-7" />
         </span>
@@ -29,12 +30,16 @@ export default function AdditionalControls() {
 
       <LanguageButton />
 
-      {/* TODO: icon */}
       <button
-        className="additional-controls-button w-max-content flex h-8 items-center justify-center rounded-full border-2 border-white bg-black px-[18px] py-[15px] text-white hover:border-black hover:bg-white hover:text-black"
+        className="additional-controls-button w-max-content group flex h-8 items-center justify-center rounded-full border-2 border-white bg-black px-[18px] py-[15px] text-white hover:border-black hover:bg-white hover:text-black"
         onClick={onLearnMoreClick}
       >
-        <span className="mr-4 text-lg">x</span>
+        <span className="mr-4 text-lg">
+          <InfoSpeechBubbleIcon
+            className="h-5 w-5 fill-white group-hover:fill-black"
+            fill="inherit"
+          />
+        </span>
         <span>Learn more</span>
       </button>
     </div>
